55545e202cd23536fff7ea34541126482e69e4c3
protheory8 <editaw5@gmail.com>
2020-08-01 15:42:05 +0000

    Allow trailing comma in macros

--- src/macros.rs
+++ src/macros.rs
@@ -4,7 +4,7 @@ macro_rules! map {
 		let temp_map = ::std::collections::HashMap::new();
 		temp_map
 	});
-	($($key:expr => $value:expr),+) => ({
+	($($key:expr => $value:expr),+ $(,)?) => ({
 		let mut temp_map = ::std::collections::HashMap::new();
 		$(
 			temp_map.insert($key, $value);
@@ -19,7 +19,7 @@ macro_rules! set {
 		let temp_set = ::std::collections::HashSet::new();
 		temp_set
 	});
-	($($x:expr),+) => ({ // Match one or more comma delimited items
+	($($x:expr),+ $(,)?) => ({ // Match one or more comma delimited items
 		let mut temp_set = ::std::collections::HashSet::new();  // Create a mutable HashSet
 		$(
 			temp_set.insert($x); // Insert each item matched into the HashSet
